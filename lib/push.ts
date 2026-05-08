7ede6041179dec380b1a9b1c192d4101019b2b53
toru-matano <116661983+toru-matano@users.noreply.github.com>
2026-05-08 09:31:10 +0900

    Add files via upload

--- lib/push.ts
+++ lib/push.ts
@@ -3,7 +3,7 @@
 export const PUSH_STORAGE_KEY = 'splitmate_push_subscription'
 
 /** Convert a base64url VAPID public key to a Uint8Array */
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = atob(base64)
